c8ccec59fc3e88ee7936bc23beb34ae36094b233
Ayesha-Aziz <moizs4644@gmail.com>
2024-09-10 12:02:18 +0500

    milestone1

--- app.ts
+++ app.ts
@@ -1,10 +1,16 @@
+
 const toggleSkillsButton = document.getElementById('toggle-skills') as HTMLButtonElement;
 const skillsList = document.getElementById('skills-list') as HTMLUListElement;
 
 toggleSkillsButton.addEventListener('click', () => {
     if (skillsList.style.display === 'none' || !skillsList.style.display) {
         skillsList.style.display = 'block';
+        skillsList.innerText = "Hide"
     } else {
         skillsList.style.display = 'none';
     }
 });
+
+
+
+
